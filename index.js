3e58e1a61e8000f0a5fc2464536e210650fd0d81
bengfarrell <ben@benfarrell.com>
2015-08-23 23:06:47 -0700

    tie into and launch the scheduler when the engine starts

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var routes     = require('./routes');
-var engine = require('shark-engine');
+var shark      = require('shark-engine');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/media', express.static(engine.config.mediaDirectory));
-app.use('/shows', express.static(engine.config.packaging.showLocation));
+app.use('/media', express.static(shark.config.mediaDirectory));
+app.use('/shows', express.static(shark.config.packaging.showLocation));
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
 routes.init(router);
 app.use('/api', router);
 app.listen(port);
+
+shark.schedule.run();
 
 console.log('Server running on ' + port);
